b59fc7510f2b610fa7fabe44795541f3ccfe185c
Coral <coralpeony21@gmail.com>
2023-01-21 20:41:50 +0200

    added function populatedata to get items from the local storage

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // save reference to important DOM elements
 var timeDisplayEl = $("#time-display");
-
-
+var confirmDiv = $("#confirm");
 
 // handle displaying the time
 function displayTime() {
@@ -11,47 +10,62 @@ function displayTime() {
 
 // Assign past present future classes function
 function assignClasses() {
-    var currentTime = moment().format("H");
-  
-    for (i = 9; i < 18; i++) {
-      //console.log(i, currentTime,"#"+i);
-      if (i < currentTime) {
-        $("#" + i).addClass("past");
-      }
-      if (i == currentTime) {
-        $("#" + i).addClass("present");
-      }
-      if (i > currentTime) {
-        $("#" + i).addClass("future");
-      }
+  var currentTime = moment().format("H");
+
+  for (i = 9; i < 18; i++) {
+    //console.log(i, currentTime,"#"+i);
+    if (i < currentTime) {
+      $("#" + i).addClass("past");
+    }
+    if (i == currentTime) {
+      $("#" + i).addClass("present");
+    }
+    if (i > currentTime) {
+      $("#" + i).addClass("future");
     }
   }
+}
 
-  // save to local storage function
+// save to local storage function
 function saveTLS(event) {
-    event.preventDefault();
-  
-    // collect data from data attr and the value from the textarea
-    const currentHour = event.target.getAttribute("data-hour");
-    var input = $(event.target).siblings("textarea").val();
-  
-    // get the todos object from ls
-    var todosFromLS = JSON.parse(localStorage.getItem("todos"));
-  
-    // add the new todo to the object (using the spread operator)
-    var todosToSave = {
-      ...todosFromLS,
-      [currentHour]: input,
-    };
-  
-    // save the updated todos back in ls
-    localStorage.setItem("todos", JSON.stringify(todosToSave));
-  
-    confirmDiv.text("Task is added to local storage ✔️");
+  event.preventDefault();
+
+  // collect data from data attr and the value from the textarea
+  const currentHour = event.target.getAttribute("data-hour");
+  var input = $(event.target).siblings("textarea").val();
+
+  // get the todos object from ls
+  var todosFromLS = JSON.parse(localStorage.getItem("todos"));
+
+  // add the new todo to the object (using the spread operator)
+  var todosToSave = {
+    ...todosFromLS,
+    [currentHour]: input,
+  };
+
+  // save the updated todos back in ls
+  localStorage.setItem("todos", JSON.stringify(todosToSave));
+
+  confirmDiv.text("Task is added to local storage ✔️");
+}
+
+// get items from the local storage
+function populateData() {
+  var todosFromLS = JSON.parse(localStorage.getItem("todos"));
+  var textareas = $("textarea");
+
+  for (let index = 0; index < textareas.length; index++) {
+    var textAreaId = textareas[index].id;
+    var todoText = todosFromLS[textAreaId];
+
+    if (todoText) {
+      $(`#${textAreaId} `).text(todoText);
+    }
   }
-  
-  
-  $(".saveBtn").on("click", saveTLS);
-  
-  assignClasses();
-  displayTime();
+}
+populateData();
+
+$(".saveBtn").on("click", saveTLS);
+
+assignClasses();
+displayTime();
