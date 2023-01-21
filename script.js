5b7b87e8c092e8c561fb78fc3a7f09f27cb470b2
Coral <coralpeony21@gmail.com>
2023-01-21 20:34:02 +0200

    added function assignClasses to colour-code block

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function displayTime() {
   var rightNow = moment().format("dddd, MMMM Do YYYY");
   timeDisplayEl.text(rightNow);
 }
+
+// Assign past present future classes function
+function assignClasses() {
+    var currentTime = moment().format("H");
+  
+    for (i = 9; i < 18; i++) {
+      //console.log(i, currentTime,"#"+i);
+      if (i < currentTime) {
+        $("#" + i).addClass("past");
+      }
+      if (i == currentTime) {
+        $("#" + i).addClass("present");
+      }
+      if (i > currentTime) {
+        $("#" + i).addClass("future");
+      }
+    }
+  }
